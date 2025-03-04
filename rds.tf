347bac1cb41ad90403636a4554057d765b645aa1
stella fordjour <stellafordjour@gmail.com>
2025-03-04 20:32:17 +0000

    three tier project

--- rds.tf
+++ rds.tf
@@ -1,7 +1,8 @@
 resource "aws_db_subnet_group" "main" {
   name       = "main"
   subnet_ids = [aws_subnet.private_db.id, aws_subnet.private_app.id]
-}
+
+  }
 
 resource "aws_db_instance" "mysql" {
   identifier        = "mysql-db"
@@ -12,4 +13,5 @@ resource "aws_db_instance" "mysql" {
   password          = "password"
   db_subnet_group_name = aws_db_subnet_group.main.name
   vpc_security_group_ids = [aws_security_group.db_sg.id]
+  
 }
